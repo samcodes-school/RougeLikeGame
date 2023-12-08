3e0c6c953f8a8cff5033746ce846aa85b830d591
Sam <sbeamish2006@gmail.com>
2023-12-08 10:23:38 -0500

    Test-Pygame Window

---
+++ Main/pygame window.py
@@ -0,0 +1,17 @@
+import pygame
+import time
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 900))
+###image=pygame.image.load ('Hades_cover_art.jpg')
+done = False
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.flip()
+    print("tick "+ str(pygame.time.get_ticks()))
+    time.sleep(1)

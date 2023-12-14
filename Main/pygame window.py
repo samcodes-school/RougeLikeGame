faf140367d1fc14b4c4c15090d8489d41021fc3e
Sam <sbeamish2006@gmail.com>
2023-12-14 11:15:33 -0500

    Final Commit, 12/13/23,Sam.B

--- Main/pygame window.py
+++ Main/pygame window.py
@@ -1,8 +1,6 @@
 import pygame
 import time
-import sys
-import os
-import math
+
 
 pygame.init()
 win = pygame.display.set_mode((1000, 900))
@@ -14,12 +12,26 @@ done = False
 win.blit(image, (0, 0))
 win.blit(image1, (500, 0))
 
-
+clock = pygame.time.Clock()
 
 while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
     pygame.display.flip()
     print("tick " + str(pygame.time.get_ticks()))
     time.sleep(1)
+
+    run = True
+    speed = 30
+    while run:
+        clock.tick(speed)
+        image -= 1.4
+        image1 -= 1.4
+
+        if image < image.get_width() * -1:
+            image = image.get_width()
+
+        if image1 < image1.get_width() * -1:
+            image1 = image1.get_width()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True

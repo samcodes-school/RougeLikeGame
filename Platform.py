b3ddafb27a354253133a4e3e60dfb01d91d4470a
tommy salter <tommysalterburrows@gmail.com>
2023-12-18 11:14:51 -0500

    collision changes

--- Platform.py
+++ Platform.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.locals import *
 import Collision
 
+##I would have liked to test this, but I don't know how to integrate it into the main code.
+
 class Platform(Rect):
     def __init__(self,xPos,yPos,width):
         self.left = xPos
@@ -9,6 +11,13 @@ class Platform(Rect):
         self.width = width
         self.height = 20
 
-    def floorTime(self,faller):
-        if Collision.collideTop(faller,self):
-            faller.bottom = self.top
+    def floorTime(self,character):
+        if Collision.collideTop(character,self):
+            character.bottom = self.top
+
+    def wallTime(self,character):
+        if Collision.collideLeft(character,self):
+            character.right = self.left
+
+        if Collision.collideRight(character,self):
+            character.left = self.left + self.width

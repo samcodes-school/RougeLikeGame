69c57299ef144d99664b69f8127dfce0ff3f6ffc
tommy salter <tommysalterburrows@gmail.com>
2023-12-19 09:40:17 -0500

    collision changes
    (cherry picked from commit 74710904412db748b0d28b4a5ad87fe603e2ae4a)

---
+++ Platform.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.locals import *
+import Collision
+
+##I would have liked to test this, but I don't know how to integrate it into the main code.
+
+
+class Platform(Rect):
+    def __init__(self,xPos,yPos,width):
+        self.left = xPos
+        self.top = yPos
+        self.width = width
+        self.height = 20
+
+    def floorTime(self,character):
+        if Collision.collideTop(character,self):
+            character.bottom = self.top
+
+    def wallTime(self,character):
+        if Collision.collideLeft(character,self):
+            character.right = self.left
+
+        if Collision.collideRight(character,self):
+            character.left = self.left + self.width
+
+    def draw(self,screen):
+        pygame.draw.rect(screen,(240,48,64),self)
--- sidescrolling.py
+++ sidescrolling.py
@@ -1,9 +1,16 @@
 import pygame
 import time
+import os
 
 pygame.init()
 win = pygame.display.set_mode((1000, 900))
-pygame.display.set_caption('Side Scroller')
+pygame.display.set_caption('Side Scroll-Test')
+
+bg = pygame.image.load(os.path.join('curling-stone-emoji-2048x1534-1ec3cb7w.png')).convert()
+bgX = 0
+bgX2 = bg.get_width()
+
+clock = pygame.time.Clock()
 
 image = pygame.image.load('curling-stone-emoji-2048x1534-1ec3cb7w.png')
 #image1 = pygame.image.load('IMG_20231212_214709_407.png')
@@ -22,20 +29,18 @@ while not done:
     speed = 30
     while run:
         clock.tick(speed)
-        image -= 1.4
+        bgX -= 1.4
         #image1 -= 1.4
 
-        if image < image.get_width() * -1:
-            image = image.get_width()
-
+        if bgX < bg.get_width() * -1:
+            bgX = bg
         #if image1 < image1.get_width() * -1:
             #image1 = image1.get_width()
+
+def redrawWindow():
+    win.blit(bg, (bgX, 0))
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
-
-# add this bg = pygame.image.load(os.path.join('images','bg.png')).convert()
-# bgX = 0
-# bgX2 = bg.get_width()

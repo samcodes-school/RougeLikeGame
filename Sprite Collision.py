016477630f8bf1e4a3547220839a0660f2edf27e
Alex Dell <adell1@ocdsb.ca>
2023-12-19 09:13:52 -0500

    Sprite Collision

---
+++ Sprite Collision.py
@@ -0,0 +1,8 @@
+#Sprite Collision
+#To do collision between sprites you have to update the self.rect values for x and y once the player has moved
+# self.rect.x=self.x
+# self.rect.y=self.y
+
+#Then you can check if the self.rect values for the player is touching something else, like a sprite
+# if pygame.sprite.collide_rect(self, sprite) and self.invincibility==0:
+#     self.health-=10
